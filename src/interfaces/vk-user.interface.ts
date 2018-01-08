f13f103406fc62648fc63e9f61a17394bcbd48f7
Arti <arti@nethouse.ru>
2018-01-08 16:56:59 +0300

    Initial commit

---
+++ src/interfaces/vk-user.interface.ts
@@ -0,0 +1,13 @@
+export interface IVKUser {
+    uid: number;
+    first_name: string;
+    last_name: string;
+    deactivated?: string;
+    hidden?: number;
+    bdate?: string;
+    sex?: number;
+    photo_50: string;
+    counters: {
+        friends: number
+    }
+}
